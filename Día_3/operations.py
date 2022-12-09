bf4cb874907c7c673ff9140e191016ce41c34a3a
Mario <marcalpozo@gmail.com>
2022-12-09 11:12:15 +0100

    Día 3 hasta ahora

---
+++ Día_3/operations.py
@@ -0,0 +1,10 @@
+age=int=15
+height=float=1.73
+ecuación1=complex= 3 + 3j
+base=input('Base')
+print(base)
+int=base
+altura=input('Altura')
+print(altura)
+int=altura
+print('el perímetro es',(0,5*base*altura))
