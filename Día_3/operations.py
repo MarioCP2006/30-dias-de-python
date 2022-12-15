b743714fad515f1ab8b5ed3a4a447c30eff7dc6f
Mario <marcalpozo@gmail.com>
2022-12-15 09:15:50 +0100

    Día 3 por ahora

--- Día_3/operations.py
+++ Día_3/operations.py
@@ -1,10 +1,35 @@
-age=int=15
+age=15
 height=float=1.73
 ecuación1=complex= 3 + 3j
-base=input('Base')
+base=int(input('Base'))
 print(base)
-int=base
-altura=input('Altura')
+altura=int(input('Altura'))
 print(altura)
-int=altura
 print('el perímetro es',(0,5*base*altura))
+
+ladoa=int(input('Lado a'))
+ladob=int(input('Lado b'))
+ladoc=int(input('Lado c'))
+perimeter=(ladoa + ladob + ladoc)
+print(perimeter)
+
+length= int(input('Largo de su rectangulo'))
+print(length)
+wide= int(input('Ancho de su rectangulo'))
+areaofrectangle=length * wide
+print(areaofrectangle)
+perimetrofrectangle=2*(length+wide)
+print(perimetrofrectangle)
+
+pi=3,14
+radioofcircle=int(input('Radio de su circulo'))
+print(radioofcircle)
+areaofcircle=pi*radioofcircle**2
+print(areaofcircle)
+circumferenceofcircle=2*pi *radioofcircle
+print(circumferenceofcircle)
+
+x=int(input('Valor de x'))
+print(x)
+y=2*x-2
+print(y)
