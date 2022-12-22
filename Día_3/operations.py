a052cabc783ac28e6f42f95bd9d41b74c0088d53
Mario <marcalpozo@gmail.com>
2022-12-22 09:13:52 +0100

    Faltan ej 16,17,18,19,20

--- Día_3/operations.py
+++ Día_3/operations.py
@@ -29,7 +29,42 @@ print(areaofcircle)
 circumferenceofcircle=2*pi *radioofcircle
 print(circumferenceofcircle)
 
-x=int(input('Valor de x'))
-print(x)
-y=2*x-2
-print(y)
+function1="y=2x-2"
+print("La formula es:"+function1)
+m_de_function1=str(2)
+print("La pendiente es igual a:"+m_de_function1)
+ejex=str(2^0 -2)
+print("el intercepto de x es igual a:")
+print(0,ejex)
+ejey=str((0+2)/2)
+print("el intercepto de y es igual a:")
+print(ejey,0)
+
+punto1=(2,2)
+punto2=(6,10)
+x1=2 in punto1
+x2=6 in punto2
+y1=2 in punto1
+y2=10 in punto2
+m=(y2-y1)/(x2-x1)
+print(f"La m es:{m}")
+euclideandist=(x2-x1)**2+(y2-y1)**2
+print(f"La distancia euclidiana es de:{euclideandist})
+
+print(m_de_function1==m)
+
+test_numbers=(2,3,-3,5,6)
+for n in test_numbers:
+    print(n**2+6*n+9)
+
+print(len("python"))
+print(len("dragon"))
+print(len("python")!=len("dragon"))
+
+print("on"in "python")
+print("on"in "dragon")
+
+print("jargon"in"I hope this course is not full of jargon")
+
+print("on" not in "dragon")
+print("on"not in "python")
