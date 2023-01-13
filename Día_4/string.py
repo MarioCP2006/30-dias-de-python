b611cc9717402a862db78621e762b5bcee3b1c9a
Mario <marcalpozo@gmail.com>
2023-01-13 11:11:12 +0100

    Día 4 empezado

---
+++ Día_4/string.py
@@ -0,0 +1,24 @@
+thirty_days_of_python="Thirty" + " " + "Days" + " " + "Of" + " " + "Python"
+print(thirty_days_of_python)
+
+coding_for_all="Coding" + " " + "For" + " " + "All"
+print(coding_for_all)
+
+company=coding_for_all
+print(company)
+print(len(company))
+
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company.replace("Coding", " "))
+print("Coding" in company)
+print(company.replace('Coding', 'Python'))
+Python_For_All=company.replace('Coding', 'Python')
+print(Python_For_All.replace('All' , 'Everyone'))
+print(company.split( ))
+marcas="Facebook" + ",  " + "Google" + ", "+"Microsoft" + ", " + "Apple" + ", " + "IBM" + ", " + "Oracle" + ", " + "Amazon"
+print(marcas.split(", "))
+
