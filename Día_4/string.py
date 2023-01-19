2c77e4937b0c1be5e61a4501bbfb71981c9f7467
Mario <marcalpozo@gmail.com>
2023-01-19 09:14:56 +0100

    Día 4 hecho

--- Día_4/string.py
+++ Día_4/string.py
@@ -13,7 +13,7 @@ print(company.lower())
 print(company.capitalize())
 print(company.title())
 print(company.swapcase())
-print(company.replace("Coding", " "))
+print(company.strip("Coding "))
 print("Coding" in company)
 print(company.replace('Coding', 'Python'))
 Python_For_All=company.replace('Coding', 'Python')
@@ -22,3 +22,18 @@ print(company.split( ))
 marcas="Facebook" + ",  " + "Google" + ", "+"Microsoft" + ", " + "Apple" + ", " + "IBM" + ", " + "Oracle" + ", " + "Amazon"
 print(marcas.split(", "))
 
+print(company[0])
+print(company[13])
+print(company[10])
+Pfa=Python_For_All[0] + Python_For_All[7] + Python_For_All [11]
+print(Pfa)
+Cfa=company[0] + company[7] + company[11]
+print(Cfa)
+print(company.index("C"))
+print(Python_For_All.index("F"))
+print(company.rindex("l"))
+frase= 'You cannot end a sentence with because because because is a conjunction'
+print(frase.find('because'))
+print(frase.rfind('because'))
+print(frase.replace(' because because because ', ' '))
+print(frase.index('because '))
